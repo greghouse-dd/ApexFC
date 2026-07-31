91ede90dd85ebe92cd16a37d2eae651ac8348e6d
Devansh Dhoka <devanshd20201@gmail.com>
2026-08-01 03:20:19 +0530

    feat: use NEXT_PUBLIC_API_URL environment variable in API configurations

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./token";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000",
 });
 
 api.interceptors.request.use((config) => {
